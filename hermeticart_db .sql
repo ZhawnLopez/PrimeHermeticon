95fcf479799d1c59b72c074b00ad816aa5f63ff9
ZhawnLopez <zblopez@student.apc.edu.ph>
2026-02-25 16:36:05 +0800

    accessibility ease for creating db

--- hermeticart_db .sql
+++ hermeticart_db .sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `hermeticart_db`
 --
-
+CREATE DATABASE IF NOT EXISTS hermeticart_db;
+USE hermeticart_db;
 -- --------------------------------------------------------
 
 --
